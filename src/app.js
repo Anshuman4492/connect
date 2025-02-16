db7ee78983c9341c77ea88787f812aaa95056312
hackerank <me@hackerank.com>
2025-02-16 11:08:12 +0530

    Created CRUD user routes and tested on postman

--- src/app.js
+++ src/app.js
@@ -2,6 +2,33 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
+
+app.post('/login',(req,res)=>{
+    res.send('Login Page')
+})
+app.post('/register',(req,res)=>{
+    res.send('Register Page')
+})
+app.get('/user',(req,res)=>{
+    res.send('User Data fetched successfully')
+})
+app.post('/user',(req,res)=>{
+    res.send('User Data saved successfully')
+})
+app.delete('/user',(req,res)=>{
+    res.send('User Data deleted successfully')
+})
+app.put('/user',(req,res)=>{
+    res.send('User Data updated successfully')
+})
+app.use('/user',(req,res)=>{
+    res.send('User Data Wildcard')
+})
+app.use('/',(req,res)=>{
+    res.send('Homepage')
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
